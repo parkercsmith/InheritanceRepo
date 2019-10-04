abcad2249c7bdd1f3581c4cd4f0844083a1729e2
parkercsmith <46355904+parkercsmith@users.noreply.github.com>
2019-10-04 15:13:30 -0400

    Add files via upload

--- OptimizedNetworkSimulation.jl
+++ OptimizedNetworkSimulation.jl
@@ -225,7 +225,7 @@ function birth(network::NetworkParameters, child::Int64, parent::Int64)
                         network.edgeMatrix[child, i] = 1
                     end
                 else
-                    if(rand() < network.popPNC[child]) #PND MODE
+                    if(rand() < network.popPND[child]) #PND MODE
                         network.edgeMatrix[i, child] = 1
                         network.edgeMatrix[child, i] = 1
                     end
@@ -333,7 +333,7 @@ function runSims(CL::Float64, BEN::Float64)
     end
     dataArray[:] ./= Float64(repSims)
     #EDIT NAME
-    save("assortmentBase_CL$(CL)_B$(BEN).jld2", "parameters", [CL, BEN], "meanPN", dataArray[1], "void", dataArray[2], "meanPR", dataArray[3], "meanDegree", dataArray[4], "meanAssortment", dataArray[5], "meanDistanceFromDefToCoop", dataArray[6], "meanDistanceInclusion", dataArray[7], "meanCooperationRatio", dataArray[8])
+    save("assortmentCD_CL$(CL)_B$(BEN).jld2", "parameters", [CL, BEN], "meanPNC", dataArray[1], "meanPND", dataArray[2], "meanPR", dataArray[3], "meanDegree", dataArray[4], "meanAssortment", dataArray[5], "meanDistanceFromDefToCoop", dataArray[6], "meanDistanceInclusion", dataArray[7], "meanCooperationRatio", dataArray[8])
 end
 
 argTab = ArgParseSettings(description = "arguments and stuff, don't worry about it")
